53032df5007721ff5fb500db3482bde638f23a5e
Ellis Goranov <me@ellis.pw>
2021-01-28 22:04:13 +0000

    set nickname property on create game

--- TicTacCore/WebSocket/Handlers/Incoming/NewGameHandler.cs
+++ TicTacCore/WebSocket/Handlers/Incoming/NewGameHandler.cs
@@ -17,11 +17,14 @@ namespace TicTacToeServer.WebSocket.Handlers.Incoming
         public void Execute(WebSocketClient SocketClient, MessageObject Data)
         {
             var gameObject = GameEnvironment.GetGameManager().CreateGame(SocketClient);
+            var nickname = Convert.ToString(Data.Body["nickname"]);
             if (!SocketClient.SetGame(gameObject))
             {
                 SocketClient.Send(new ErrorHandler("Already in game"));
                 return;
             }
+
+            SocketClient.Nickname = nickname;
             SocketClient.Send(new EstablishNewGameHandler(gameObject));
             SocketClient.Send(new UpdateBoardHandler(gameObject));
         }
