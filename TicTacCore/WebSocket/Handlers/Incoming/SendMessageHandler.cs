5e8a2058871df48af5924a743660d47bb96d9ad9
Ellis Goranov <me@ellis.pw>
2021-01-28 22:47:11 +0000

    remove incomingeventtype, setup incomingevent for chatbox handling

---
+++ TicTacCore/WebSocket/Handlers/Incoming/SendMessageHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToeServer.WebSocket.Handlers.Outgoing;
+using TicTacToeServer.WebSocket.Utilities;
+
+namespace TicTacCore.WebSocket.Handlers.Incoming
+{
+    public class SendMessageHandler : IncomingEvent
+    {
+        public string Identifier => "SEND_MESSAGE";
+
+        public void Execute(WebSocketClient SocketClient, MessageObject Data)
+        {
+            var message = Convert.ToString(Data.Body["message"]);
+            if (message.Contains('<') || message.Contains('>'))
+            {
+                SocketClient.Send(new ErrorHandler("Chat message can't contain special characters"));
+                return;
+            }
+
+
+        }
+    }
+}
